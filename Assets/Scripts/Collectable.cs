64c9d16ff2ee52fa28f7fa967a10d0ed0af50c99
JohnLi002 <44849474+JohnLi002@users.noreply.github.com>
2022-03-15 00:40:37 -0400

    created a collectable

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public GameObject obtainEffect;
+    public Player p;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if(collider.tag == "Player")
+        {
+            p.increaseCollectableCount();
+            Instantiate(obtainEffect, new Vector3(transform.position.x, transform.position.y - 0.3f, -0.3f), Quaternion.identity);
+            GameObject.Destroy(gameObject);
+
+        }
+    }
+}
